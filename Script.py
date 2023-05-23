a8d223bbc63cdf615bc43983a7fa255cce404de8
RykovStas <stasyan_2003@icloud.com>
2023-05-23 09:40:23 -0400

    Update to xls with updating data

--- Script.py
+++ Script.py
@@ -1,19 +1,22 @@
 import os
 import time
-from openpyxl import Workbook
 import requests
 from fake_useragent import UserAgent
+from openpyxl import Workbook, load_workbook
 
 
-
+# User Agent
 ua = UserAgent()
 url = os.environ.get("URL")
 password = os.environ.get('Pass')
-data_list = []
-workbook = Workbook()
-worksheet = workbook.active
-
 headers = {'User-Agent': ua.chrome}
+
+# WorkBook Excel
+workbook = Workbook()
+sheet = workbook.active
+
+
+# Connection to main server
 resp = requests.get(url, headers=headers, verify=False, timeout=5)
 data = resp.json()
 sid = data['sid']
@@ -21,7 +24,6 @@ sid = data['sid']
 
 # Validations & Get SID
 if resp.status_code == 200:
-    data = resp.json()
     sid = data['sid']
     print(f"Your sid is {sid}")
 else:
@@ -32,28 +34,50 @@ def find_dict(lst, key, value):
     return [d for d in lst if key in d and d[key] == value]
 
 
-while True:
-    cheks = f"https://10.79.3.10:8080/pos_events?password={password}"
-    chek = requests.get(cheks, headers=headers, verify=False, timeout=5)
-    datacheks = chek.json()
-    chek.raise_for_status()
-    if chek.status_code == 200:
-        print("All is OK!")
+# Transactions
+transactions = f"https://10.79.3.10:8080/pos_events?password={password}"
+sheet.append(['Reciept No.', 'POS Terminal', 'Price'])
+existing_data = []
+if os.path.exists('data.xlsx'):
+    existing_workbook = load_workbook('data.xlsx')
+    existing_sheet = existing_workbook.active
 
+    # Validation of data
+    for row in existing_sheet.iter_rows(values_only=True):
+        existing_data.append(row)
+
+while True:
+    transaction = requests.get(transactions, headers=headers, verify=False, timeout=5)
+    data = transaction.json()
+    transaction.raise_for_status()
+    if transaction.status_code == 200:
+        print("All is OK!")
+    else:
+        print("Connection Wrong!")
 
     target_key = 'type'
-    target_value = 'POSNG_RECEIPT_FINAL_RESULT'  #  type is always == "POSNG_RECEIPT_FINAL_RESULT
+    target_value = 'POSNG_RECEIPT_SELL_CLOSE'  # Type is always == "POSNG_RECEIPT_SELL_CLOSE"
 
+    result = find_dict(data, target_key, target_value)
 
-    result = find_dict(datacheks, target_key, target_value)
-    print(result)
-    for item in result:
-        if item['value'] not in data_list:
-            worksheet.append([item['key'], item['value']])
-            data_list.append(item['value'])
+    if result:
+        for item in result:
+            # Take data
+            value1 = item.get('op_id')
+            value2 = item.get('pos_terminal_name')
+            value3 = item.get('price')
 
-    workbook.save('output.xlsx')
+            if value3 is not None:
+                value3 = str(value3)
+                if len(value3) > 2:
+                    value3 = value3[:-2] + '.' + value3[-2:]
 
-    time.sleep(1)
+            if (value1, value2, value3) not in existing_data:
 
+                # Add data to Excel
+                sheet.append([value1, value2, value3])
+                existing_data.append((value1, value2, value3))
 
+        workbook.save('data.xlsx')
+
+    time.sleep(60)
